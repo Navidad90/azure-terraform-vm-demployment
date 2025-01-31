e1645c1268de1f2e40c61d18dc7631c6f9cab9d3
Navid.Rasul <58747987+Navidad90@users.noreply.github.com>
2025-01-31 20:03:33 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "Contoso-resourcegroup-europe"
-  location = "germanywestcentral"
-}
-
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ContosoVirtualNetwork"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "ContosoSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
