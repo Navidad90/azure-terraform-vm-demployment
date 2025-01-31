e5c9b903d8ae3a89ab09dcaf14ee1fe8949b0e2d
Navid.Rasul <58747987+Navidad90@users.noreply.github.com>
2025-01-31 20:06:06 +0100

    variables.tf

---
+++ nsg/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "germanywestcentral"
+}
+
+variable "resource_group_name" {
+  default = "Contoso-resourcegroup-europe"
+}
+
+variable "nsg_name" {
+  default = "ContosoNSG"
+}
